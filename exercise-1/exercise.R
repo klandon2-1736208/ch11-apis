bf19891cd25a075f24fc41aaa47fd3ddbbd0608c
Kcee Landon <klandon2@uw.edu>
2018-04-18 17:15:13 -0700

    Edit exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -8,6 +8,7 @@ library(dplyr)
 
 # Create a variable for the API's base URI (https://api.github.com)
 
+base_uri <- "https://api.github.com"
 
 # Under the "Repositories" category of the API, 
 # find the endpoint that will list repos in an organization. Then, 
@@ -15,18 +16,25 @@ library(dplyr)
 # organization repos (this is the PATH to the resource of interest).
 # (FYI: this is where we keep the book code and master exercise sets!)
 
+resource <- "/orgs/info201/repos"
 
 # Send a GET request to this endpoint (the `base_uri`` followed by `resource`)
 
+uri_full <- paste0(base_uri, resource)
+
+response <- GET(uri_full)
 
 # Extract the "text" of the response usin the `content` function
 
+response_content <- content(response, type = "text")
 
 # Convert the body from JSON into a data frame
 
+body_data <- fromJSON(response_content)
 
 # How many (public) repositories does the organization have?
 
+repo_num <- nrow(body_data)
 
 ##### New query ######
 
@@ -35,14 +43,18 @@ library(dplyr)
 # (hint: https://developer.github.com/v3/search/#search-repositories)
 # Reassign the `resource` variable to refer to the appropriate resource.
 
+resource <- "/search/repositories"
 
 # You will need to specify some query parameters. Create a `query_params` list 
 # variable that specifies an appropriate key and value for the search term and
 # the language
 
-
+query_params <- list(q = "?graphics+language:R")
+  
+  
 # Send a GET request to this endpoint--including your params list as the `query`
 
+response <- get(paste0(base_uri, resource, query_params))
 
 # Extract the response body and convert it from JSON.
 
